d411c9adaf13a3d9c46b494954dd054c5f35772e
John Murray <heyjohnmurray@us.ibm.com>
2018-02-19 01:01:18 -0600

    add first component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,12 @@
+// import React so we can extend its Component method
+import React from 'react';
+
+// StorePicker component
+class StorePicker extends React.Component {
+    render() {
+        return <p>Hello</p>
+    }
+}
+
+// make it available throughout the app
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+import {render} from 'react-dom';
+
+// import StorePicker component
+import StorePicker from './components/StorePicker';
+
+// instantiate component's tag and select where we're mounting the component
+render (<StorePicker/>, document.querySelector('#main'));
