2de74346cb99c3846b86b5f7fa188c3bed4828b2
John Murray <heyjohnmurray@us.ibm.com>
2018-02-24 00:23:16 -0600

    making sure I understand how to pass dynamic content

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,7 @@ class App extends React.Component{
     render(){
         return <div className="app">
             <Header tagContent="This is the header"/>
+            <Header tagContent="This is also a header, but with its own tag!" subHead="I'm a subhead, too"/>
         </div>
     }
 }
--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Header extends React.Component{
     render (){
-        return <div className="header">{this.props.tagContent}</div>
+        return <div className="header">{this.props.tagContent}<br/>{this.props.subHead}</div>
     }
 }
 
